f5d774c8bc767dc7a0a359ca2b064703b443cf79
Alexander Horstman <alexander.horstman@yahoo.com>
2015-11-12 11:09:40 -0800

    Push before reset.
    Removed some unnecessary methods, comments, and variables in various
    classes. Methods commented out are not working. Some will be removed
    later.

--- src/main/resources/static/js/index-control-ajs.js
+++ src/main/resources/static/js/index-control-ajs.js
@@ -3,17 +3,8 @@
 var cs480App = angular.module('AddBookmark', []);
 
 cs480App.controller('AddBookmarkCtrl', function ($scope, $http) {
-  
-	var increment = (function() {
-		  var count = 1;
-		  
-		  return (function() {
-			  return count++;
-		  })
-	})();
 	
     $scope.addBookmark = function() {
-	  var count = increment();
 	  var div = document.createElement('div');
 
 	  div.className = 'large-3 small-3 columns end';
@@ -29,12 +20,12 @@ cs480App.controller('AddBookmarkCtrl', function ($scope, $http) {
 		    </div>';
 
 	  document.getElementById('content').appendChild(div);
-  	} 
+  	}
   
 	$http.post("cs480/user/" + "user1" + "/" + count + "?bookmark=" + $scope.newBookmark)
 		.success(function(data){
 	  		$scope.loadUsers();
-	});
+	})
 	
 	$scope.loadPages = function() {
 		  $http.get("preset1/pages")
@@ -42,5 +33,51 @@ cs480App.controller('AddBookmarkCtrl', function ($scope, $http) {
 		  		$scope.pageitems = data;
 		  	});
 	  }
+	
+	$scope.addPreset1 = function() {
+		alert('alert');
+		$http.get("cs480/users/list").success(function(data) {
+			$scope.addPreset();
+		});
+	}
 
+	function addPreset() {
+		alert('alert');
+		  var div = document.createElement('div');
+		  var presetName = document.getElementById('presetName').value;
+		  if(presetName == '') {
+			  //do nothing
+			  
+		  }
+		  else {
+			  div.className = 'large-3 small-3 columns end';
+
+			    div.innerHTML = '&nbsp;\
+					&nbsp;\
+			    	&nbsp;\
+			    	&nbsp;\
+			    	<ul class="stack button-group">\
+			    	<li><a href="#" class="button large" onClick="openUrls(' + presetName + ')" >' + presetName +'</a></li>\
+			    	<li><a href="get-preset-ajs.html?user=user1&bookmark=0" class="button small">Edit</a></li>\
+			    	</ul>\
+			    	</div>';
+
+			     document.getElementById('content').appendChild(div);
+			     
+		  }
+		    
+	}
+
+	function openUrls(name) {
+		  alert(name);
+	}
+
+	//use angular
+	function getUrls(name) {
+		  //get a list of the urls from the user.bookmark
+		  //call openURLS by passing list of URLS to it
+	}
+
+	$scope.loadUsers();
 });
+
